0e1742c859e8994f8706ef6c462ef81ebb5bb982
ahaines10592 <35705974+ahaines10592@users.noreply.github.com>
2019-01-20 23:42:09 -0600

    Update Lucky7s.js

--- Lucky7s.js
+++ Lucky7s.js
@@ -12,33 +12,31 @@ var moneyEarned = [initialBet];
 var win = 4;
 var loss = 1;
 var maxEarned = 0;
-
 	
-
-	do{
+do{
 		
-		rollCount++;
-		die1 = Math.floor(Math.random() * 6) + 1;
-		die2 = Math.floor(Math.random() * 6) + 1;	
-		dice = dice1+dice2;
+	rollCount++;
+	die1 = Math.floor(Math.random() * 6) + 1;
+	die2 = Math.floor(Math.random() * 6) + 1;	
+	dice = dice1+dice2;
 
 			
-			if (dice == 7){
-			money = money+win; // 
-			var last_element = moneyEarned[moneyEarned.length -1]; 
-			var last_updated = last_element + win; 
-			moneyEarned.push(last_updated);
+	if (dice == 7){
+	money = money+win; // 
+	var last_element = moneyEarned[moneyEarned.length -1]; 
+	var last_updated = last_element + win; 
+	moneyEarned.push(last_updated);
 
 			
-			}else {
-			money--;
-			Counter++;
+	}else {
+	money--;
+	Counter++;
 				
-			var last_element = moneyEarned[moneyEarned.length -1];
-			var last_updated = last_element - loss;
-			moneyEarned.push(last_updated);
-			rollCount = rollCount++;
-			}
+	var last_element = moneyEarned[moneyEarned.length -1];
+	var last_updated = last_element - loss;
+	moneyEarned.push(last_updated);
+	rollCount = rollCount++;
+	}
 
 
 	}while(money>0);
@@ -47,10 +45,10 @@ var maxEarned = 0;
 	totalMax = Math.max.apply(Math, moneyEarned); 
 	numRolls = moneyEarned.indexOf(totalMax); 
 	if(numRolls<1){
-		numRolls = 0;
-	}
+	numRolls = 0;
+		}
 
 	document.getElementById("p1").innerHTML=("<center><br><br><table style=\"width: 100% height: 100%;\"><th colspan=\"2\"; class=\"th1\">Results</th><tr><th class=\"th2\">Starting Bet</th><th class=\"th2\">$" + initialBet + ".00</th></tr><tr><td>Total Rolls Before Going Broke</td><td>"+ rollTally +"</td></tr><tr><td>Highes Amount Won</td><td>"+totalMax+"</td></tr><tr><td>Roll Count at Highest Amount Held</td><td>"+ numRolls + "</td></tr></table></center>");
-	document.getElementById("p2").innerHTML="Play Again";
+	document.getElementById("p2").innerHTML="Retry";
 
 }
